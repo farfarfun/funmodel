22dc82bf231759e36ca0d3fc6ede5a8239f4411c
farfun <1007530194@qq.com>
2024-03-20 19:48:47 +0800

    add

--- funmodel/core/predict/base.py
+++ funmodel/core/predict/base.py
@@ -4,7 +4,7 @@ import os
 class PredictModel:
     def __init__(self, model_name="tmp", cache_dir=None, *args, **kwargs):
         self.model_name = model_name
-        self.cache_dir = cache_dir or f"{os.getcwd()}/.funmodel/"
+        self.cache_dir = cache_dir or f"{os.environ['HOME'] or os.getcwd()}/.funmodel/"
         os.makedirs(self.cache_path, exist_ok=True)
 
     @property
